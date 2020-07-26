de7dc0423948ab6af4e8f9493d363b0a570a729b
HanGwangUk <baramukki@naver.com>
2020-07-27 00:12:42 +0900

    table 지정

--- skeleton-project/frontend/src/pages/Recommend/Beginner/day1.jsx
+++ skeleton-project/frontend/src/pages/Recommend/Beginner/day1.jsx
@@ -11,9 +11,14 @@ import {
   makeStyles,
   Paper,
 } from '@material-ui/core';
-import TestImage from './img/TestImage.jpg';
+import chestpress from './img/chestpress.jpg';
 import tbarrow from './img/tbarrow.png';
 import './styles.css';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -32,12 +37,64 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+function createData(pic, exp, prog) {
+  return { pic, exp, prog };
+}
+
+const rows = [
+  createData(
+    <img className="photo" src={tbarrow}></img>,
+    <p>
+      등 운동aaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      aaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaa
+    </p>,
+    '10 * 5',
+  ),
+  createData(
+    <img className="photo" src={chestpress}></img>,
+    <p>가슴운동 </p>,
+    '10 * 5',
+  ),
+];
+
 export default function Day1() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <Grid container>
+      <div>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell align="center" width="50%">
+                운동종류
+              </TableCell>
+              <TableCell align="center" width="25%">
+                운동소개
+              </TableCell>
+              <TableCell align="center" width="25%">
+                추천프로그램 <br /> 횟수 * set
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map(row => (
+              <TableRow>
+                <TableCell align="center" width="50%">
+                  {row.pic}
+                </TableCell>
+                <TableCell align="center">{row.exp}</TableCell>
+                <TableCell align="center">{row.prog}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+}
+{
+  /* <Grid container>
         <Grid item xs={6}>
           <Paper className={classes.imgpaper}>
             <img className="photo" src={TestImage}></img>
@@ -80,7 +137,5 @@ export default function Day1() {
             dddddddddddddddddddddddddddddddddddddd
           </Paper>
         </Grid>
-      </Grid>
-    </div>
-  );
+      </Grid> */
 }
