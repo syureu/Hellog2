1945ba9086f6a8e69f0b59fd7fe4d5fe8e6472ee
HanGwangUk <baramukki@naver.com>
2020-07-30 23:31:43 +0900

    운동추천 방식 modal로 변경을 위한 샘플

---
+++ skeleton-project/frontend/src/pages/Recommend/Advanced/mdt.jsx
@@ -0,0 +1,132 @@
+import React from 'react';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import chest from './chestpress.jpg';
+import './styles.css';
+
+import Modal from '@material-ui/core/Modal';
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  table: {
+    maxWidth: 1200,
+  },
+}));
+
+const StyledTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles(theme => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+function createData(pic, exp, prog) {
+  return { pic, exp, prog };
+}
+
+const rows = [
+  createData(
+    <div>ChestPress</div>,
+    <div>
+      <h2>ChestPress</h2>머신을 이용해 가슴 중앙을 운동함
+    </div>,
+    '10 * 5',
+  ),
+  createData(<div>운동2</div>, <p>가슴운동 </p>, '10 * 5'),
+];
+
+export default function MDT() {
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <img className="photo" src={chest}></img>
+    </div>
+  );
+  return (
+    <div>
+      <h1>가 슴 운 동</h1>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="center" width="50%">
+                운동 종류
+              </StyledTableCell>
+              <StyledTableCell align="center" width="25%">
+                운동 설명
+              </StyledTableCell>
+              <StyledTableCell align="center" width="25%">
+                추천 프로그램 <br />
+                횟수 * set 수
+              </StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map(row => (
+              <StyledTableRow key={row.pic}>
+                <StyledTableCell align="center">
+                  <button type="button" onClick={handleOpen}>
+                    {row.pic}
+                  </button>
+                  <Modal open={open} onClose={handleClose}>
+                    {body}
+                  </Modal>
+                </StyledTableCell>
+                <StyledTableCell align="center">{row.exp}</StyledTableCell>
+                <StyledTableCell align="center">{row.prog}</StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+}
