3ba4d4239372e00710383fbd54385dcf6f044b96
전상혁 <houek0307@naver.com>
2020-07-30 17:53:14 +0900

    S03P12D203-56 [EDIT] Article page edit

--- skeleton-project/frontend/server/server.js
+++ skeleton-project/frontend/server/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send(`서버 실행!!`);
+app.get('/api/host', (req, res) => {
+  res.send({ host: 'hellog' });
 });
 
 app.listen(PORT, () => {
