e146fef6159fb577e6e2b80a9328d9cdfa4a5db0
HanGwangUk <baramukki@naver.com>
2020-07-30 09:21:28 +0900

    S03{12D203-77 버튼수정| 백엔드와 회원가입 양식 세팅 필요

--- user-create-loginout/src/containers/Base/HeaderContainer.js
+++ user-create-loginout/src/containers/Base/HeaderContainer.js
@@ -5,6 +5,34 @@ import * as userActions from "redux/modules/user";
 import { bindActionCreators } from "redux";
 import storage from "lib/storage";
 
+import styled from "styled-components";
+import oc from "open-color";
+import { Link } from "react-router-dom";
+import { shadow } from "lib/styleUtils";
+
+const BorderedButton = styled(Link)`
+  font-weight: 600;
+  color: ${oc.cyan[6]};
+  border: 1px solid ${oc.cyan[6]};
+  padding: 0.5rem;
+  padding-bottom: 0.4rem;
+  cursor: pointer;
+  border-radius: 2px;
+  text-decoration: none;
+  transition: 0.2s all;
+
+  &:hover {
+    background: ${oc.cyan[6]};
+    color: white;
+    ${shadow(1)}
+  }
+
+  &:active {
+    /* 마우스 클릭시 아래로 미세하게 움직임 */
+    transform: translateY(3px);
+  }
+`;
+
 class HeaderContainer extends Component {
   handleLogout = async () => {
     const { UserActions } = this.props;
@@ -27,7 +55,9 @@ class HeaderContainer extends Component {
         {user.get("logged") ? (
           <div>
             {user.getIn(["loggedInfo", "username"])}{" "}
-            <div onClick={this.handleLogout}>(로그아웃)</div>
+            <BorderedButton onClick={this.handleLogout}>
+              로그아웃
+            </BorderedButton>
           </div>
         ) : (
           <LoginButton />
