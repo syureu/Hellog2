04b4cc3b6258484277a3c9b9f6ca6ca079efd188
Syureu <tor01237@gmail.com>
2020-08-04 15:10:14 +0900

    S03P13D203-29 [EDIT] equipment 테이블 점검 준비 sql 원형 폴더 ddl로 바꿈

---
+++ backend/src/main/resources/DDL/equipment.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `equipment` (
+  `equipment_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `gym_id` bigint(20) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`equipment_id`),
+  KEY `equipment_FK` (`gym_id`),
+  CONSTRAINT `equipment_FK` FOREIGN KEY (`gym_id`) REFERENCES `gym` (`gym_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
