f6f5b44e54c99bc312bfbb82432dd54eaeb92d21
Syureu <tor01237@gmail.com>
2020-08-11 14:53:58 +0900

    S03P13D203-108 [ADD] dietboard db check

---
+++ backend/src/main/java/com/ssafy/pjt1track3/dietboard/dietBoard.java
@@ -0,0 +1,79 @@
+package com.ssafy.pjt1track3.dietboard;
+
+import java.util.Date;
+
+public class dietBoard {
+    Long dietBoardContentId;
+    String title;
+    Long writer;
+    Date writeTime;
+    Date editTime;
+    String content;
+    Byte[] picture;
+
+    public dietBoard(Long dietBoardContentId, String title, Long writer, Date writeTime, Date editTime, String content, Byte[] picture) {
+        this.dietBoardContentId = dietBoardContentId;
+        this.title = title;
+        this.writer = writer;
+        this.writeTime = writeTime;
+        this.editTime = editTime;
+        this.content = content;
+        this.picture = picture;
+    }
+
+    public Long getDietBoardContentId() {
+        return dietBoardContentId;
+    }
+
+    public void setDietBoardContentId(Long dietBoardContentId) {
+        this.dietBoardContentId = dietBoardContentId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getWriter() {
+        return writer;
+    }
+
+    public void setWriter(Long writer) {
+        this.writer = writer;
+    }
+
+    public Date getWriteTime() {
+        return writeTime;
+    }
+
+    public void setWriteTime(Date writeTime) {
+        this.writeTime = writeTime;
+    }
+
+    public Date getEditTime() {
+        return editTime;
+    }
+
+    public void setEditTime(Date editTime) {
+        this.editTime = editTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Byte[] getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Byte[] picture) {
+        this.picture = picture;
+    }
+}
