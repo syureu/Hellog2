c3ae6ac233768f317ba2703a0b7292ffe58f9d94
skdewww <skdewww@gmail.com>
2020-08-14 15:57:15 +0900

    S03P13D203-150 [add] carousel manual

--- material-dashboard-react-master/material-dashboard-react-master/src/views/Test/Test.js
+++ material-dashboard-react-master/material-dashboard-react-master/src/views/Test/Test.js
@@ -10,7 +10,7 @@ import gym_unsplash3 from "../Test/gym_unsplash3.jpg";
 export default function Test() {
   return (
     <div>
-      <Carousel>
+      <Carousel interval={null}>
         <Carousel.Item>
           <img className="d-block w-100" src={gym_unsplash} alt="First slide" />
           <Carousel.Caption>
@@ -40,9 +40,11 @@ export default function Test() {
           <Carousel.Caption>
             <h3>Third slide label</h3>
             <p>
+              <a href="/login">
+                <Button className="btn btn-light">로그인</Button>
+              </a>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
             </p>
-            <Button variant="light">회원가입</Button>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
