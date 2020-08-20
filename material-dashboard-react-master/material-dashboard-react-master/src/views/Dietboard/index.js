cd679f804646c8000bc2daabac9891bb356437cf
Sanghyeok-Jeon <sh_jeon@kakao.com>
2020-08-21 01:08:07 +0900

    S03P13D203-155 [EDIT] Frontend CSS Final

--- material-dashboard-react-master/material-dashboard-react-master/src/views/Dietboard/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Main } from "./main";
-export { default as Write } from "./write";
-export { default as List } from "./list";
-export { default as Search } from "./search";
-export { default as View } from "./view";
